7b1c243b5ab0774ead94fb932aaa26334be28b9e
wyne-ybanez <wyne_01@icloud.com>
2021-11-01 21:21:07 +0000

    Add setup styling and app forms

---
+++ src/Alert.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Alert = () => {
+  return <h2>Alert component</h2>
+}
+
+export default Alert
--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import List from './List'
+import Alert from './Alert'
 
 function App() {
+  const [item, setItem] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editID, setEditID] = useState(null)
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('Hello')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="Add your item here"
+            value={item}
+            onChange={(e) => setItem(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">Clear Items</button>
+      </div>
+    </section>
+  )
 }
 
-export default App;
+export default App
