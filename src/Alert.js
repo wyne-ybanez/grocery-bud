89d53837ae4f4e5392d41e4ec872d645e99b0721
wyne-ybanez <wyne_01@icloud.com>
2021-11-06 20:51:59 +0000

    Add alert logic for when user does not enter an input

--- src/Alert.js
+++ src/Alert.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-const Alert = () => {
-  return <h2>Alert component</h2>
+const Alert = ({ type, msg, removeAlert }) => {
+  // time out after a few seconds
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert()
+    }, 4000)
+    return () => clearTimeout(timeout)
+  }, [])
+  return <p className={`alert alert-${type}`}>{msg}</p>
 }
 
 export default Alert
--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ function App() {
   const [list, setList] = useState([])
   const [isEditing, setIsEditing] = useState(false)
   const [editID, setEditID] = useState(null)
-  const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: '',
+    type: '',
+  })
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log('Hello')
     if (!item) {
       // display alert
+      showAlert(true, 'danger', 'please enter an item')
     } else if (item && isEditing) {
       // deal with edit
     } else {
@@ -23,10 +28,14 @@ function App() {
     }
   }
 
+  const showAlert = (show = false, type = '', msg = '') => {
+    setAlert({ show, type, msg }) //default values
+  }
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <div className="form-control">
           <input
             type="text"
