cff30e516561a4bab91bf6c1644e5b3886928853
wyne-ybanez <wyne_01@icloud.com>
2021-11-07 16:41:16 +0000

    Add remove item logic and refactor timeout alert display

--- src/Alert.js
+++ src/Alert.js
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react'
 
-const Alert = ({ type, msg, removeAlert }) => {
+const Alert = ({ type, msg, list, removeAlert }) => {
   // time out after a few seconds
   useEffect(() => {
     const timeout = setTimeout(() => {
       removeAlert()
     }, 3500)
     return () => clearTimeout(timeout)
-  }, [])
+  }, [list])
   return <p className={`alert alert-${type}`}>{msg}</p>
 }
 
--- src/App.js
+++ src/App.js
@@ -38,10 +38,18 @@ function App() {
     setList([])
   }
 
+  const removeItem = (id) => {
+    /* Display items that does not have the id we are passing through,
+       hence, filter for items without the id passed in */
+
+    showAlert(true, 'danger', 'item removed')
+    setList(list.filter((item) => item.id !== id))
+  }
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
         <div className="form-control">
           <input
             type="text"
@@ -57,7 +65,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} />
+          <List items={list} removeItem={removeItem} />
           <button className="clear-btn" onClick={clearList}>
             Clear Items
           </button>
